37c6ad4f6dc2405fe051c38181854bf846054a23
Jeova BL <jeovab115@gmail.com>
2025-02-13 17:54:22 -0300

    add novas dependencias

--- api_data_sales/main.py
+++ api_data_sales/main.py
@@ -1,14 +1,8 @@
 from fastapi import FastAPI
-import uvicorn
 
 app = FastAPI()
 
-@app.get("/")
-async def read_root():
-    return {"Hello": "World"}
 
-def run():
-    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
-
-if __name__ == "__main__":
-    run()
+@app.get('/')
+def read_root():
+    return {'message': 'Olá Mundo!'}
