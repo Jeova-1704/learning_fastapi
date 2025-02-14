d94ab1beadff6cdd682ef3543be55fb86353de3d
Jeova BL <jeovab115@gmail.com>
2025-02-14 01:12:55 -0300

    Adicionada a primeira migração com Alembic. Criada tabela de usuários.

--- api_data_sales/main.py
+++ api_data_sales/main.py
@@ -33,11 +33,13 @@ def read_users():
     return {'users': database}
 
 
-@app.put('/users/{user_id}', response_model=UserPublic, status_code=HTTPStatus.OK)
+@app.put(
+    '/users/{user_id}', response_model=UserPublic, status_code=HTTPStatus.OK
+)
 def update_user(user_id: int, user: UserSchema):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
     user_with_id = UserDb(**user.model_dump(), id=user_id)
     database[user_id - 1] = user_with_id
@@ -45,13 +47,15 @@ def update_user(user_id: int, user: UserSchema):
     return user_with_id
 
 
-@app.delete("/users/{user_id}", response_model=Message, status_code=HTTPStatus.OK)
+@app.delete(
+    '/users/{user_id}', response_model=Message, status_code=HTTPStatus.OK
+)
 def delete_user(user_id: int):
     if user_id > len(database) or user_id < 1:
         raise HTTPException(
-            status_code=HTTPStatus.NOT_FOUND, detail="User not found"
+            status_code=HTTPStatus.NOT_FOUND, detail='User not found'
         )
 
     del database[user_id - 1]
 
-    return {"message": "User deleted"}
+    return {'message': 'User deleted'}
